0746f0d2a3dd4cb2242be8baed83a7b9d0944354
elize <elizesmyrnova@gmail.com>
2023-06-06 23:59:36 +0200

    DZ 11

---
+++ src/test/java/my/test/DZ_12_TeachersList.java
@@ -0,0 +1,38 @@
+package my.test;
+import Util.util;
+import config.Baseclass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import java.util.List;
+
+public class DZ_12_TeachersList extends Baseclass {
+    static String mainLink = "https://dnipro.ithillel.ua/";
+    By coursesOpen = By.xpath("//*[@id=\"body\"]/div[1]/div[1]/div[2]/div/div/nav/ul/li[2]/button");
+    By programuvannya = By.xpath("//*[@id=\"coursesMenuControlPanel\"]/ul/li[2]/button");
+    By frontEndBasic = By.xpath("//*[@id=\"block-202987\"]/div/ul/li[1]/a");
+    By listOfTeachers = By.xpath("//li[@class=\"coach-list_item\"]");
+
+
+    @Test
+    public void showTeachersList() {
+        driver.get(mainLink);
+        driver.findElement(coursesOpen).click();
+        driver.findElement(programuvannya).click();
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(frontEndBasic).click();
+
+        util.scrollToElementVisibilityOf(driver, By.className("coach-card_name"));
+
+        List<WebElement> wList = driver.findElements(listOfTeachers);
+        System.out.println("Course Front-End Basic: list of teachers: \n");
+        for (WebElement element : wList) {
+            System.out.println(element.findElement(By.className("coach-card_name")).getText());
+        }
+    }
+}
+
